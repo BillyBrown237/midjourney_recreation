8615306577bbfa5ca4553fea4c28657313c7a976
dazehinn <dazehinn@yahoo.fr>
2023-12-28 14:53:34 +0100

    Add SearchBar component to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Feed from "./components/Feed";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <Feed />
     </div>
   );
